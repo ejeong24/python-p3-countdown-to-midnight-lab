b228a646699bdb5634aa173f999d9a1e33a1abe2
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-05-23 22:04:23 -0400

    countdown() and countdown_with_sleep() working

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,24 @@
-# your code goes here!
+import time
+
+def countdown(from_this_number):
+    start=from_this_number
+    while (start > 0):
+        if (start > 0):
+            print(f'{start} SECOND(S)!')
+            start-=1
+        if (start == 0):
+            print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(from_this_number, for_how_many_seconds=1):
+    start=from_this_number
+    while (start > 0):
+        if (start > 0):     
+            print(f'{start} SECOND(S)!')
+            start-=1
+            time.sleep(for_how_many_seconds)
+        if (start == 0):
+            print('HAPPY NEW YEAR!')
+
+countdown(10)
+countdown_with_sleep(10,1)
+        
